ab0a4a0c783410571c12b1748b2fce601d041a7d
Kim Kline <kim@dialexa.com>
2021-02-26 16:42:38 -0600

    add user favorite migration

---
+++ db/migrations/20210226223830_add_user_favorite.ts
@@ -0,0 +1,29 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('user_favorites', (tableBuilder) => {
+    tableBuilder
+      .uuid('id')
+      .notNullable()
+      .primary()
+      .defaultTo(knex.raw('gen_random_uuid()'));
+
+    tableBuilder.timestamps(true, true);
+
+    tableBuilder
+      .uuid('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users');
+
+    tableBuilder
+      .uuid('contestant_id')
+      .notNullable()
+      .references('id')
+      .inTable('contestants');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('user_favorites');
+}
